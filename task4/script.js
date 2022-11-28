41a5d7d18e20d2563019026efcd5285389f31109
VladimirGoncharuk <ximer_ra@mail.ru>
2022-11-29 02:25:54 +0300

    doc

--- task4/script.js
+++ task4/script.js
@@ -1,6 +1,6 @@
 const link1 = document.querySelector ('#link');
 
 link1.addEventListener ('click',function(){
-    this.textContent=prompt('измините название ссылки');
+    this.textContent=prompt('измените название ссылки');
 });
 
